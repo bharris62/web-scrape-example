692edffbea6353ebfa830273e4992de8f967072b
Blake Harris <blakebharris@gmail.com>
2016-09-29 20:36:07 -0700

    added functionality

--- scrape.py
+++ scrape.py
@@ -8,5 +8,21 @@ soup = BeautifulSoup(r.content)
 
 links = soup.find_all('a')
 
-for link in links:
-    print("<a href='%s'>%s</a>" %(link.get('href'), link.text))
+# for link in links:
+#     print("<a href='%s'>%s</a>" %(link.get('href'), link.text))
+
+
+gen_data = soup.find_all('div', {'class': 'info'})
+
+for item in gen_data:
+    print(item.contents[0].find_all('a', {'class': 'business-name'})[0].text)
+    print(item.contents[1].find_all('p', {'class': 'adr'})[0].text)
+    try:
+        print(item.contents[1].find_all('span', {'itemprop': 'address'})[0].text)
+        print(item.contents[1].find_all('span', {'itemprop': 'addresslocality'})[0].text)
+    except:
+        pass
+    try:
+        print(item.contents[1].find_all('li', {'class': 'primary'})[0].text)
+    except:
+        pass
