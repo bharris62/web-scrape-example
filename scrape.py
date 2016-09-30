160da8a4944ccb40d1ae2d7b0061ca21ae166325
Blake Harris <blakebharris@gmail.com>
2016-09-29 20:16:47 -0700

    basic struc

---
+++ scrape.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.yellowpages.com/search?search_terms=coffee&geo_location_terms=nashville%2C+tn'
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content)
+
+links = soup.find_all('a')
+
+for link in links:
+    print("<a href='%s'>%s</a>" %(link.get('href'), link.text))
